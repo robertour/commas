536aa284b5265036c4f7392fce5c0e33204a2df8
Roberto Ulloa <Roberto Ulloa>
2013-10-10 01:39:27 +0800

    tweaking the design

--- commas/settings.py
+++ commas/settings.py
@@ -33,6 +33,8 @@ DATABASES = {
     }
 }
 
+INTERNAL_IPS = ['127.0.0.1']
+
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
